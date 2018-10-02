2786d6412cb88a3b322ae9e17d7e82e423884805
Gideon Clegg <19.gideon.clegg@ksd.org>
2018-10-02 09:18:08 -0700

    Update 10/02

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+    public int health = 3;
+    public int shield = 0;
+    private bool invulnerable = false;
+    private float time = 0;
+    private float flashTimer = 0;
+    public float invulnerableTime = 1;
+    public Sprite[] sprite_health;
+    public Sprite[] sprite_shield;
+    public Image health_image;
+    public Image shield_image;
+    private bool gameOver = false;
+    public float gameOverTime = 3;
+    public float time2 = 0;
+    private float speed;
+    private static bool created = false;
+    GameObject player;
+    PlayerMovement playerMovement;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        playerMovement = player.GetComponent<PlayerMovement>();
+        speed = playerMovement.speed;
+    }
+    private void Awake()
+    {
+        if (!created)
+        {
+            DontDestroyOnLoad(gameObject);
+            created = true;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        if (gameOver)
+        {
+            playerMovement.speed = 0;
+            time2 += Time.deltaTime;
+            if (time2 >= gameOverTime)
+            {
+                SceneManager.LoadScene("Scene1");
+                playerMovement.speed = speed;
+                invulnerable = false;
+                health = 3;
+                gameOver = false;
+            }
+        }
+        if (invulnerable && !gameOver)
+        {
+            flashTimer += Time.deltaTime;
+            if (flashTimer < 0.1f)
+            {
+                Vector4 color = gameObject.GetComponent<SpriteRenderer>().color;
+                color.w = 0.8f;
+                gameObject.GetComponent<SpriteRenderer>().color = color;
+            } else if (flashTimer>0.1f && flashTimer < 02f)
+            {
+                Vector4 color = gameObject.GetComponent<SpriteRenderer>().color;
+                color.w = 1f;
+                gameObject.GetComponent<SpriteRenderer>().color = color;
+            }
+            else
+            {
+                flashTimer = 0f;
+            }
+            time += Time.deltaTime;
+            if (time >= invulnerableTime)
+            {
+                Vector4 color = gameObject.GetComponent<SpriteRenderer>().color;
+                color.w = 1f;
+                gameObject.GetComponent<SpriteRenderer>().color = color;
+                invulnerable = false;
+                time = 0;
+            }
+        }
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy" && invulnerable == false)
+        {
+            if (shield > 0)
+            {
+                shield--;
+           //     shield_image.sprite = sprite_shield[shield];
+            }
+            else
+            {
+                health--;
+                //      health_image.sprite = sprite_health[health];
+            }
+            invulnerable = true;
+            if (health <= 0)
+            {
+                gameOver = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed = 3.0f;
+    //private Animator animator;
+	// Use this for initialization
+	void Start () {
+      //  animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+       /* if (Mathf.Abs(y) < Mathf.Abs(x))
+        {
+            if (x > 0) { animator.SetTrigger("MoveRight"); }
+            if (x < 0) { animator.SetTrigger("MoveLeft"); }
+        }
+        else
+        {
+            if (y > 0) { animator.SetTrigger("MoveUp"); }
+            if (y < 0) { animator.SetTrigger("MoveDown"); }
+        }
+        if (x == 0 && y == 0) { animator.SetTrigger("MoveNone"); }*/
+        Vector2 velocity = new Vector2(x, y);
+        GetComponent<Rigidbody2D>().velocity = velocity * speed;
+    }
+}
